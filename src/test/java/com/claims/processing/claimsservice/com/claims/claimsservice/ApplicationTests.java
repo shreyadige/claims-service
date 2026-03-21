fe4dfcb55390d98776a2ff5c78eea2d27ce82118
Shreya Dige <shreyadige101@gmail.com>
2026-03-21 12:29:59 +0530

    Initial commit

---
+++ src/test/java/com/claims/processing/claimsservice/com/claims/claimsservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claims.processing.claimsservice.com.claims.claimsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
